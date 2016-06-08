29efa16c08943ac623e4717712f4eac2a9fb919d
choiseul <chs9135@gmail.com>
2016-06-08 19:34:07 +0900

    Add Category/feel List

---
+++ Client-Android/app/src/main/java/kr/kookmin/myprivatechef/model/Category.java
@@ -0,0 +1,226 @@
+package kr.kookmin.myprivatechef.model;
+
+import java.util.ArrayList;
+
+public class Category {
+
+    private ArrayList<Integer> code;
+    private ArrayList<String> title;
+    private ArrayList<Integer> ckCategory;
+
+    public Category(){
+        code = new ArrayList<>();
+        code.add(48196);
+        code.add(48197);
+        code.add(48198);
+        code.add(48199);
+        code.add(48200);
+        code.add(48201);
+        code.add(48202);
+        code.add(48203);
+        code.add(48204);
+        code.add(48205);
+        code.add(48206);
+        code.add(48207);
+        code.add(48208);
+        code.add(48209);
+        code.add(48210);
+        code.add(48211);
+        code.add(48212);
+        code.add(48213);
+        code.add(48214);
+        code.add(48215);
+        code.add(48216);
+        code.add(48217);
+        code.add(48218);
+        code.add(48219);
+        code.add(48220);
+        code.add(48221);
+        code.add(48222);
+        code.add(48223);
+        code.add(48224);
+        code.add(48225);
+        code.add(48226);
+        code.add(48228);
+        code.add(48234);
+        code.add(48235);
+        code.add(48236);
+        code.add(48392);
+        code.add(48393);
+        code.add(48394);
+        code.add(48395);
+        code.add(48397);
+        code.add(48399);
+        code.add(48400);
+        code.add(48401);
+        code.add(48402);
+        code.add(48403);
+        code.add(48404);
+        code.add(48233);
+        code.add(48172);
+        code.add(48196);
+        code.add(48197);
+        code.add(48198);
+        code.add(48199);
+        code.add(48200);
+        code.add(48201);
+        code.add(48202);
+        code.add(48203);
+        code.add(48204);
+        code.add(48205);
+        code.add(48206);
+        code.add(48207);
+        code.add(48208);
+        code.add(48209);
+        code.add(48210);
+        code.add(48211);
+        code.add(48212);
+        code.add(48213);
+        code.add(48214);
+        code.add(48215);
+        code.add(48216);
+        code.add(48217);
+        code.add(48218);
+        code.add(48219);
+        code.add(48220);
+        code.add(48221);
+        code.add(48222);
+        code.add(48223);
+        code.add(48224);
+        code.add(48225);
+        code.add(48226);
+        code.add(48228);
+        code.add(48234);
+        code.add(48235);
+        code.add(48236);
+        code.add(48392);
+        code.add(48393);
+        code.add(48394);
+        code.add(48395);
+        code.add(48397);
+        code.add(48399);
+        code.add(48400);
+        code.add(48401);
+        code.add(48402);
+        code.add(48403);
+        code.add(48404);
+        code.add(48233);
+        code.add(48172);
+
+        title = new ArrayList<>();
+        title.add("ë°¥ë¥˜");
+        title.add("ì£½ë¥˜");
+        title.add("ë©´ë¥˜");
+        title.add("ë§Œë‘");
+        title.add("êµ???);
+        title.add("ì°Œê°œ/?„ê³¨");
+        title.add("êµ¬ì´");
+        title.add("ì¡°ë¦¼");
+        title.add("ë³¶ìŒ");
+        title.add("ë¬´ì¹¨");
+        title.add("?€ê¹€");
+        title.add("ë¶€ì¹?);
+        title.add("ì°??¶ê¸°");
+        title.add("?‰ì±„");
+        title.add("ê¹€ì¹?);
+        title.add("?“ê°ˆ");
+        title.add("?¥ì•„ì°?);
+        title.add("?¼í´");
+        title.add("?ëŸ¬??);
+        title.add("?˜í”„");
+        title.add("?¼ì");
+        title.add("?¤íŒŒê²Œí‹°/?ŒìŠ¤?€");
+        title.add("?¤í…Œ?´í¬");
+        title.add("??);
+        title.add("?œë ˆ???ŒìŠ¤");
+        title.add("??);
+        title.add("?œê³¼");
+        title.add("ë¹?);
+        title.add("?Œë“œ?„ì¹˜/? ìŠ¤??);
+        title.add("ë¬´ìŠ¤/?¸ë”©/?¬ë¦¼");
+        title.add("ê³¼ì");
+        title.add("ì°??¤ë¥˜");
+        title.add("?¥ë¥˜");
+        title.add("ì¡°ë?ë£??¥ì‹ ë£?);
+        title.add("ê°€ë£¨ë¥˜");
+        title.add("ì£¼ìŠ¤");
+        title.add("ê±´ê°•?Œë£Œ");
+        title.add("?¤ë¬´???ì´??);
+        title.add("?€???¬ë„ì£?);
+        title.add("ë§¥ì£¼");
+        title.add("?ì£¼/?™ë™ì£??„í†µì£?);
+        title.add("ê¸°í?ì£¼ë¥˜");
+        title.add("ë¹™ìˆ˜");
+        title.add("?„ì´?¤í¬ë¦?);
+        title.add("ì¹˜ì¦ˆ");
+        title.add("?”êµ¬ë¥´íŠ¸");
+        title.add("ê¸°í??Œë£Œë¥?ë¹™ê³¼ë¥?? ì œ??);
+        title.add("ê¸°í? ?”ë¦¬ë³??ˆì‹œ??);
+        title.add("ë°¥ë¥˜");
+        title.add("ì£½ë¥˜");
+        title.add("ë©´ë¥˜");
+        title.add("ë§Œë‘");
+        title.add("êµ???);
+        title.add("ì°Œê°œ/?„ê³¨");
+        title.add("êµ¬ì´");
+        title.add("ì¡°ë¦¼");
+        title.add("ë³¶ìŒ");
+        title.add("ë¬´ì¹¨");
+        title.add("?€ê¹€");
+        title.add("ë¶€ì¹?);
+        title.add("ì°??¶ê¸°");
+        title.add("?‰ì±„");
+        title.add("ê¹€ì¹?);
+        title.add("?“ê°ˆ");
+        title.add("?¥ì•„ì°?);
+        title.add("?¼í´");
+        title.add("?ëŸ¬??);
+        title.add("?˜í”„");
+        title.add("?¼ì");
+        title.add("?¤íŒŒê²Œí‹°/?ŒìŠ¤?€");
+        title.add("?¤í…Œ?´í¬");
+        title.add("??);
+        title.add("?œë ˆ???ŒìŠ¤");
+        title.add("??);
+        title.add("?œê³¼");
+        title.add("ë¹?);
+        title.add("?Œë“œ?„ì¹˜/? ìŠ¤??);
+        title.add("ë¬´ìŠ¤/?¸ë”©/?¬ë¦¼");
+        title.add("ê³¼ì");
+        title.add("ì°??¤ë¥˜");
+        title.add("?¥ë¥˜");
+        title.add("ì¡°ë?ë£??¥ì‹ ë£?);
+        title.add("ê°€ë£¨ë¥˜");
+        title.add("ì£¼ìŠ¤");
+        title.add("ê±´ê°•?Œë£Œ");
+        title.add("?¤ë¬´???ì´??);
+        title.add("?€???¬ë„ì£?);
+        title.add("ë§¥ì£¼");
+        title.add("?ì£¼/?™ë™ì£??„í†µì£?);
+        title.add("ê¸°í?ì£¼ë¥˜");
+        title.add("ë¹™ìˆ˜");
+        title.add("?„ì´?¤í¬ë¦?);
+        title.add("ì¹˜ì¦ˆ");
+        title.add("?”êµ¬ë¥´íŠ¸");
+        title.add("ê¸°í??Œë£Œë¥?ë¹™ê³¼ë¥?? ì œ??);
+        title.add("ê¸°í? ?”ë¦¬ë³??ˆì‹œ??);
+
+    }
+
+    public ArrayList<Integer> setCkCategory(boolean[] ckdata){
+        ckCategory = new ArrayList<>();
+        for(int i=0; i<48; i++){
+            if(ckdata[i])
+                ckCategory.add(code.get(i));
+        }
+        return ckCategory;
+    }
+
+    public int getSize(){
+        return code.size();
+    }
+
+    public String getTitle(int i){
+        return title.get(i);
+    }
+}
